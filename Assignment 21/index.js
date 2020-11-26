36bf79e25a5b15f77f6fee3bb4a9f24a484eac22
Arnav Tyagi <69686731+arnav-tyagi29@users.noreply.github.com>
2020-11-26 11:30:14 +0530

    Add files via upload

---
+++ Assignment 21/index.js
@@ -0,0 +1,8 @@
+a=prompt("Firstly, we will talk about cube's volume. Enter a value for side?");
+alert(a*a*a);
+b=prompt("Then we will discuss cuboid's area. Enter a value for length");
+c=prompt("Breadth=")
+d=prompt("Height=")
+alert(2*b*c + 2*c*d + 2*b*d)
+e=prompt("Now we will discuss about circle's circumference. Enter a value for radius.")
+alert(2*3.14*e)
